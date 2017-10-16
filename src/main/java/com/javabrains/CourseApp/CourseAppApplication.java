e34ae45cfb816a8f52819504cf9f745472b779ec
Sabaresh Lakshminarasimhan <sabaresh@apple.com>
2017-10-15 23:14:38 -0700

    saving initial project

---
+++ src/main/java/com/javabrains/CourseApp/CourseAppApplication.java
@@ -0,0 +1,12 @@
+package com.javabrains.CourseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAppApplication.class, args);
+	}
+}
